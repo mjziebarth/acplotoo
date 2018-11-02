80be8cc20b8cd334972afcb7f7fb45065ceb935b
Malte Ziebarth <malte.ziebarth@fmvkb.de>
2018-11-02 09:59:01 +0100

    Formatting.

--- plot_tools/sphereplot.py
+++ plot_tools/sphereplot.py
@@ -178,7 +178,8 @@ class Sphereplot:
 		ax.set_xlim([-1.01,1.01])
 		ax.set_ylim([-1.01,1.01])
 		ax.set_aspect('equal')
-	
+
+
 	def great_circle(self, lon1, lat1, lon2, lat2,
 	                 tolerance=None, seg_len=None, **kwargs):
 		"""
@@ -251,7 +252,7 @@ class Sphereplot:
 		"""
 		x,y,z = convert_coordinates_3d(np.array(lon), np.array(lat),
 		                               self.view_center)
-		
+
 		# Masking for color arguments etc.
 		mask = z >= 0.0
 		if not np.all(mask):
@@ -266,14 +267,14 @@ class Sphereplot:
 				c = kwargs['c']
 				if isinstance(c,np.ndarray) and len(c.shape) > 1:
 					kwargs['c'] = c[mask,...]
-			
+
 			# Mask coordinates:
 			x = x[mask]
 			y = y[mask]
-		
+
 		return self.ax.scatter(x, y, **kwargs)
-	
-	
+
+
 	def line(self, lon1, lat1, lon2, lat2, seg_len=None, 
 	          **kwargs):
 		"""
@@ -290,20 +291,20 @@ class Sphereplot:
 		                (Default: None)
 		   **kwargs   : Passed to matplotlib LineCollection.
 		"""
-		
+
 		# Override seg_len if requested:
 		SL = self._handle_seg_len(seg_len)
-		
+
 		# Create the line coordinates:
 		x,y,z = _line_coords(lon1, lat1, lon2, lat2, SL,
 		                     self.view_center)
-	
+
 		# Mask:
 		mask = z >= 0.0
 		lines = LineCollection([np.array([x[mask],y[mask]]).T], **kwargs)
 		return self.ax.add_collection(lines)
-	
-	
+
+
 	def triangle(self, lon0, lat0, lon1, lat1, lon2, lat2,
 	             seg_len=None, **kwargs):
 		"""
@@ -321,10 +322,10 @@ class Sphereplot:
 		              (Default: None)
 		   **kwargs : Passed to matplotlib Polygon.
 		"""
-		
+
 		# Override seg_len if requested:
 		SL = self._handle_seg_len(seg_len)
-		
+
 		# Get coordinates of the three lines:
 		x0,y0,z0 = _line_coords(lon0, lat0, lon1, lat1, SL,
 		                        self.view_center)
@@ -332,26 +333,26 @@ class Sphereplot:
 		                        self.view_center)
 		x2,y2,z2 = _line_coords(lon2, lat2, lon0, lat0, SL,
 		                        self.view_center)
-	
+
 		# Connect lines:
 		x = np.concatenate([x0,x1,x2])
 		y = np.concatenate([y0,y1,y2])
 		z = np.concatenate([z0,z1,z2])
-	
+
 		# Clip the points at backside of sphere:
 		x,y = _spherically_clip_polygon(x, y, z, SL)
 		if x is None:
 			# Nothing visible!
 			return
-	
+
 		# Plot:
 		handles = []
 		poly = Polygon(np.array([x,y]).T, **kwargs)
 		handles += [self.ax.add_patch(poly)]
-	
+
 		return handles
-	
-	
+
+
 	def disk(self, lon, lat, r, seg_len=None, radius_angle=None, **kwargs):
 		"""
 		Plot a disk centered on point (lon, lat) with radius r.
@@ -372,15 +373,15 @@ class Sphereplot:
 		   **kwargs     : Passed to matplotlib Polygon, 'linewidth'
 		                  also passed to radius_angle LineCollection.
 		"""
-		
+
 		# Check the radius (currently limited to 90°):
 		if r > 90.0:
 			raise ValueError("Sphereplot.disk currently only handles r<=90° "
 			                 "correctly!")
-		
+
 		# Override seg_len if requested:
 		SL = self._handle_seg_len(seg_len)
-		
+
 		# Create radius line:
 		if radius_angle is not None:
 			n = int(np.ceil(r/SL))
@@ -390,37 +391,37 @@ class Sphereplot:
 			axis = np.array(convert_coordinates_3d(0.0, 90.0,
 			                self.view_center)).reshape((3,))
 			xr,yr,zr = rotate_vectors(xr,yr,zr, axis, radius_angle)
-		
+
 		# Create circle around north pole:
 		n = int(np.ceil(360*np.sin(np.deg2rad(r))/SL))
 		lons, lats = np.linspace(0,360,n), np.ones(n)*(90.0-r)
 		x,y,z = convert_coordinates_3d(lons, lats, self.view_center)
-		
+
 		# Rotate circle to latitude:
 		axis = np.array(convert_coordinates_3d(90.0, 0.0, self.view_center))\
 		       .reshape((3,))
 		x,y,z = rotate_vectors(x,y,z, axis, 90.0-lat)
 		if radius_angle is not None:
 			xr,yr,zr = rotate_vectors(xr,yr,zr, axis, 90.0-lat)
-		
+
 		# Rotate circle to longitude:
 		axis = np.array(convert_coordinates_3d(0.0, 90.0, self.view_center))\
 		       .reshape((3,))
 		x,y,z = rotate_vectors(x,y,z, axis, lon)
 		if radius_angle is not None:
 			xr,yr,zr = rotate_vectors(xr,yr,zr, axis, lon)
-			
+
 		# Clip the points at backside of sphere:
 		x,y = _spherically_clip_polygon(x, y, z, SL)
 		if x is None:
 			# Nothing visible!
 			return
-		
+
 		# Plot:
 		handles = []
 		poly = Polygon(np.array([x,y]).T,**kwargs)
 		handles += [self.ax.add_patch(poly)]
-		
+
 		# Plot radius:
 		if radius_angle is None:
 			handles += [None]
@@ -430,11 +431,10 @@ class Sphereplot:
 				kwargs_radius["linewidth"] = kwargs["linewidth"]
 			lines = LineCollection([np.array([xr,yr]).T], color='k', **kwargs_radius)
 			handles += [self.ax.add_collection(lines)]
-		
-		
+
 		return handles
-	
-	
+
+
 	def disk_sector(self, lon, lat, r, azi0, azi1, seg_len=None,
 	                mode='sector', **kwargs):
 		"""
@@ -458,7 +458,7 @@ class Sphereplot:
 		"""
 		# Override seg_len if requested:
 		SL = self._handle_seg_len(seg_len)
-		
+
 		# Create the circle section so that it only has to be rotated
 		# in latitude direction:
 		delta_lon = (azi1-azi0) % 360.0
@@ -469,17 +469,17 @@ class Sphereplot:
 		# The coordinates of the circle section:
 		circ_lon = np.mod(np.linspace(lon0, lon1, Nc), 360.0)
 		circ_lat = (90.0-r) * np.ones(Nc)
-	
+
 		if mode == 'segment':
 			# Convert to Euclidean coordinates:
 			x,y,z = convert_coordinates_3d(circ_lon, circ_lat,
 					                       self.view_center)
-		
+
 			# Connect the end points of the arc directly without going to
 			# the center of the circle:
 			x2,y2,z2 = _line_coords(lon1, 90.0-r, lon0, 90.0-r, SL,
 			                        self.view_center)
-		
+
 			# Combined coordinates of the polygon:
 			x = np.concatenate([x, x2])
 			y = np.concatenate([y, y2])
@@ -491,31 +491,31 @@ class Sphereplot:
 			l1_lat = np.linspace(90.-r,90.,Nl)[::-1]
 			l2_lon = lon1*np.ones(Nl)
 			l2_lat = np.linspace(90.-r,90.,Nl)
-	
+
 			# Combined coordinates of the polygon:
 			lon_poly = np.concatenate([l1_lon, circ_lon, l2_lon])
 			lat_poly = np.concatenate([l1_lat, circ_lat, l2_lat])
-	
+
 			# Convert to Euclidean coordinates:
 			x,y,z = convert_coordinates_3d(lon_poly, lat_poly,
 					                       self.view_center)
-	
+
 		# Rotate to target latitude:
 		axis = np.array(convert_coordinates_3d(lon-90.0, 0.0, self.view_center))\
 		       .reshape((3,))
 		x,y,z = rotate_vectors(x,y,z, axis, -(90.0-lat))
-		
+
 		# Clip the points at backside of sphere:
 		x,y = _spherically_clip_polygon(x, y, z, SL)
 		if x is None:
 			# Nothing visible!
 			return
-	
+
 		# Plot:
 		handles = []
 		poly = Polygon(np.array([x,y]).T,**kwargs)
 		handles += [self.ax.add_patch(poly)]
-	
+
 		return handles
 	
 	
@@ -549,10 +549,10 @@ class Sphereplot:
 		# Check whether an intersection exists:
 		if great_circle_distance(lon1, lat1, lon2, lat2) > r:
 			return
-		
+
 		# Override seg_len if requested:
 		SL = self._handle_seg_len(seg_len)
-	
+
 		# Create circle around north pole:
 		n = int(np.ceil(360*np.sin(np.deg2rad(r))/SL))
 		lons, lats = np.linspace(0,360,n), np.ones(n)*(90.0-r)
@@ -564,22 +564,22 @@ class Sphereplot:
 		                                     self.view_center)
 		xc2,yc2,zc2 = convert_coordinates_3d(lon2, lat2,
 		                                     self.view_center)
-	
+
 		# Rotate circles to latitude:
 		axis = np.array(convert_coordinates_3d(90.0, 0.0, self.view_center))\
 		       .reshape((3,))
 		x1,y1,z1 = rotate_vectors(x1,y1,z1, axis, 90.0-lat1)
 		x2,y2,z2 = rotate_vectors(x2,y2,z2, axis, 90.0-lat2)
-	
+
 		# Rotate circles to longitude:
 		axis = np.array(convert_coordinates_3d(0.0, 90.0, self.view_center))\
 		       .reshape((3,))
 		x1,y1,z1 = rotate_vectors(x1,y1,z1, axis, lon1)
 		x2,y2,z2 = rotate_vectors(x2,y2,z2, axis, lon2)
-	
+
 		# Combine circles:
 		x,y,z = np.concatenate([x1,x2]),np.concatenate([y1,y2]),np.concatenate([z1,z2])
-	
+
 		# Filter out all points close enough to both centers:
 		thresh_dotp = np.cos(np.deg2rad(r+delta_r))
 		dotp1 = x*xc1 + y*yc1 + z*zc1
@@ -588,13 +588,13 @@ class Sphereplot:
 		x = connect_masked_sequence(x,mask)
 		y = connect_masked_sequence(y,mask)
 		z = connect_masked_sequence(z,mask)
-			
+
 		# Clip the points at backside of sphere:
 		x,y = _spherically_clip_polygon(x, y, z, SL)
 		if x is None:
 			# Nothing visible!
 			return
-	
+
 		# Plot:
 		handles = []
 		vertices = np.array([x,y]).T
@@ -613,7 +613,7 @@ class Sphereplot:
 			kwargs1["hatch"]=None
 			poly1 = Polygon(vertices,**kwargs1)
 			handles += [self.ax.add_patch(poly1)]
-		
+
 		return handles
 	
 	
@@ -639,7 +639,7 @@ class Sphereplot:
 		"""
 		# Override seg_len if requested:
 		SL = self._handle_seg_len(seg_len)
-		
+
 		# Create the circle section so that it only has to be rotated
 		# in latitude direction:
 		delta_lon = (azi1-azi0) % 360.0
@@ -650,26 +650,26 @@ class Sphereplot:
 		# The coordinates of the circle section:
 		circ_lon = np.mod(np.linspace(lon0, lon1, Nc), 360.0)
 		circ_lat = (90.0-r) * np.ones(Nc)
-	
+
 		# Convert to Euclidean coordinates:
 		x,y,z = convert_coordinates_3d(circ_lon, circ_lat,
 			                           self.view_center)
-	
+
 		# Rotate to target latitude:
 		axis = np.array(convert_coordinates_3d(lon-90.0, 0.0, 
 		                self.view_center)).reshape((3,))
 		x,y,z = rotate_vectors(x,y,z, axis, -(90.0-lat))
-		
+
 		# Clip the points at backside of sphere:
 		x,y = _spherically_clip_polygon(x, y, z, SL)
 		if x is None:
 			# Nothing visible!
 			return
-	
+
 		# Plot:
 		return self.ax.plot(x, y, **kwargs)
-	
-	
+
+
 	def bounds(self, lonmin, lonmax, latmin, latmax, seg_len=None, **kwargs):
 		"""
 		Plot a polygon indicating coordinate bounds on a
@@ -688,12 +688,12 @@ class Sphereplot:
 		"""
 		# Override seg_len if requested:
 		SL = self._handle_seg_len(seg_len)
-		
+
 		# We have four border lines with three different lengths:
 		N_meridian  = int(np.ceil((latmax-latmin) / SL))
 		N_col_north = int(np.ceil(np.cos(np.deg2rad(latmax))*(lonmax-lonmin) / SL))
 		N_col_south = int(np.ceil(np.cos(np.deg2rad(latmin))*(lonmax-lonmin) / SL))
-		
+
 		# Create the bounding line:
 		lon = np.concatenate([np.linspace(lonmin,lonmax,N_col_north),
 		                      lonmax*np.ones(N_meridian),
@@ -703,19 +703,19 @@ class Sphereplot:
 		                      np.linspace(latmin,latmax,N_meridian)[::-1],
 		                      latmin*np.ones(N_col_south),
 		                      np.linspace(latmin,latmax,N_meridian)])
-		
+
 		# Convert the coordinates:
 		x,y,z = convert_coordinates_3d(lon, lat, self.view_center)
-			
+
 		# Clip the points at backside of sphere:
 		x,y = _spherically_clip_polygon(x, y, z, SL)
 		if x is None:
 			# Nothing visible!
 			return
-		
+
 		# Plot:
 		poly = Polygon(np.array([x,y]).T, **kwargs)
-		
+
 		return self.ax.add_patch(poly)
 
 
@@ -747,13 +747,13 @@ class Sphereplot:
 		lat = np.linspace(-90, 90, lat_ticks)
 		lon = np.concatenate([lon, lon[-1]+ [lon[1]-lon[0]]])
 		line_segments = []
-		
+
 		# View center:
 		if vc_override is not None:
 			vc = vc_override
 		else:
 			vc = self.view_center
-		
+
 		for i in range(lon_ticks):
 			for j in range(lat_ticks):
 				if j != 0 and j != lat_ticks-1:
@@ -768,19 +768,19 @@ class Sphereplot:
 					x,y = convert_coordinates(lon_loc, lat_loc, vc)
 					if len(x) > 1:
 						line_segments += [np.array([x,y]).T]
-		
+
 		handles = []
 		lines = LineCollection(line_segments, **kwargs)
 		handles += [self.ax.add_collection(lines)]
-	
+
 		# Plot boundary:
 		n_b = lon_ticks*ticks_between
 		x,y = np.sin(np.linspace(0,2*np.pi,n_b)), np.cos(np.linspace(0,2*np.pi,n_b))
 		handles += [self.ax.add_collection(LineCollection([np.array([x,y]).T], **kwargs))]
-		
+
 		return handles
-	
-	
+
+
 	def project(self, lon, lat, three_d=False):
 		"""
 		Return projection coordinates of some lon-lat points.
@@ -795,10 +795,10 @@ class Sphereplot:
 		"""
 		if three_d:
 			return convert_coordinates_3d(lon, lat, self.view_center)
-		
+
 		return convert_coordinates(lon, lat, self.view_center)
-	
-		
+
+
 	def _handle_seg_len(self, seg_len):
 		"""
 		Handle override seg_len keyword.
@@ -814,9 +814,10 @@ class Sphereplot:
 				                "convertible to float!")
 		else:
 			SL = self.seg_len
-		
+
 		return SL
-	
+
+
 	def _handle_tolerance(self, tolerance):
 		"""
 		Handle override tolerance.
@@ -831,5 +832,5 @@ class Sphereplot:
 				                "convertible to float!")
 		else:
 			TOL = self.tolerance
-		
+
 		return TOL
