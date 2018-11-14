7367dd903a389655003d6431fb3efaf7e1172f4f
Malte Ziebarth <malte.ziebarth@fmvkb.de>
2018-11-14 12:36:03 +0100

    Handle both coordinates in Projection unit vectors

--- plot_tools/projection/projection.py
+++ plot_tools/projection/projection.py
@@ -42,6 +42,9 @@ class Projection():
 			raise ValueError("Either 'lon' and 'lat' or 'x' and 'y' have to be "
 			                 "given!")
 
+		if lon is None:
+			lon,lat = self._projection.inverse(x,y)
+
 		if hasattr(self, "_unit_vector_east"):
 			return self._unit_vector_east(lon=lon, lat=lat, x=x, y=y)
 
@@ -56,6 +59,9 @@ class Projection():
 		   or (lon is None == x is None):
 			raise ValueError("Either 'lon' and 'lat' or 'x' and 'y' have to be "
 			                 "given!")
+
+		if lon is None:
+			lon,lat = self._projection.inverse(x,y)
 
 		if hasattr(self, "_unit_vector_north"):
 			return self._unit_vector_north(lon, lat)
