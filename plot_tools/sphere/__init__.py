427c8b4bc4f66cae56351fa365f0f90ec4e03b4a
Malte Ziebarth <malte.ziebarth@fmvkb.de>
2018-11-19 17:15:37 +0100

    Delete old plot_tools directory

--- plot_tools/sphere/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# Plot tools sphere submodule module init file.
-# Copyright (C) 2018 Malte Ziebarth
-# 
-# This software is distributed under the MIT license.
-# See the LICENSE file in this repository.
-
-from .sphere import *
