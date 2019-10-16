d01da6a1ebff1fc3db3fcd785191aa9ebdde6fb8
Malte Jörn Ziebarth <malte.ziebarth@fmvkb.de>
2019-10-16 11:46:05 +0200

    Add missing field2color. Enable edgecolor for rotated text.

--- acplotoo/geoplot_base/helpers.py
+++ acplotoo/geoplot_base/helpers.py
@@ -11,10 +11,12 @@ from matplotlib.transforms import Affine2D
 from matplotlib.textpath import TextPath
 from matplotlib.patches import PathPatch
 from matplotlib import rcParams
+from matplotlib.colors import Normalize
+from matplotlib.cm import ScalarMappable
 
 
 def rotated_text(text, pos, rotation, anchor, color, zorder, size=None,
-                 margin_rel=0.15):
+                 margin_rel=0.15, edgecolor='none'):
 	"""
 	Create a rotated text a certain position.
 
@@ -54,4 +56,31 @@ def rotated_text(text, pos, rotation, anchor, color, zorder, size=None,
 	transform.rotate_deg(-rotation).translate(pos[0],pos[1])
 
 	return PathPatch(path.transformed(transform), facecolor=color,
-	                 edgecolor='none', zorder=zorder)
+	                 edgecolor=edgecolor, zorder=zorder)
+
+
+
+def field2color(field, cmap, **kwargs):
+	"""
+	Convert a scalar field to RGBA values using
+	a color map.
+	"""
+	kwargs = dict(kwargs)
+	# Extract possibly relevant keyword arguments:
+	vmin = kwargs.pop("vmin",None)
+	vmax = kwargs.pop("vmax",None)
+
+	# Normalization:
+	if vmin is None:
+		vmin = field.min()
+	if vmax is None:
+		vmax = field.max()
+	norm = Normalize(vmin, vmax)
+	c = norm(field)
+
+	# Create scalar mappable and obtain the color values
+	# from color map:
+	mappable = ScalarMappable(norm, cmap)
+	mappable.set_array(field)
+
+	return mappable.to_rgba(field), mappable
